08a04be68ec25530bcc086a7595cfdd2f59debaa
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2013-11-07 00:21:32 -0200

    refatoracao

--- src/main/java/br/com/casadocodigo/redis/capitulo3/ArmazenarHistoricoDaMegaSena.java
+++ src/main/java/br/com/casadocodigo/redis/capitulo3/ArmazenarHistoricoDaMegaSena.java
@@ -6,20 +6,29 @@ public class ArmazenarHistoricoDaMegaSena {
 
 	public static void main(String[] args) {
 		String dataDoSorteio1 = "04-09-2013";
-		String numerosDoSorteio1 = "2, 13, 24, 41, 42, 44";
-		String chave1 = String.format("resultado:megasena:%s", dataDoSorteio1);
+		String numerosDoSorteio1 = "10, 11, 18, 42, 55, 56";
+		String chave1 = String.format("resultado:%s:megasena", dataDoSorteio1);
 
 		String dataDoSorteio2 = "07-09-2013";
 		String numerosDoSorteio2 = "2, 21, 30, 35, 45, 50";
-		String chave2 = String.format("resultado:megasena:%s", dataDoSorteio2);
+		String chave2 = String.format("resultado:%s:megasena", dataDoSorteio2);
 
-		String dataDoSorteio3 = "11-09-2013";
-		String numerosDoSorteio3 = "7, 15, 20, 23, 30, 41";
-		String chave3 = String.format("resultado:megasena:%s", dataDoSorteio3);
+		String dataDoSorteio3 = "21-09-2013";
+		String numerosDoSorteio3 = "2, 13, 24, 41, 42, 44";
+		String chave3 = String.format("resultado:%s:megasena", dataDoSorteio3);
 		
+		String dataDoSorteio4 = "02-10-2013";
+		String numerosDoSorteio4 = "7, 15, 20, 23, 30, 41";
+		String chave4 = String.format("resultado:%s:megasena", dataDoSorteio4);
+
 		Jedis jedis = new Jedis("localhost");
-		String resultado = jedis.mset(chave1, numerosDoSorteio1, chave2, numerosDoSorteio2, chave3, numerosDoSorteio3);
-		
+		String resultado = jedis.mset(
+				chave1, numerosDoSorteio1,
+				chave2, numerosDoSorteio2,
+				chave3, numerosDoSorteio3,
+				chave4, numerosDoSorteio4
+		);
+
 		System.out.println(resultado);
 	}
 }
