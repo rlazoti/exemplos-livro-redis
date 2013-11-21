6ae3b5c7b47a7a7457e0689ba5e6d7decd826d96
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2013-11-20 23:23:37 -0200

    novo exemplo do cap 4

--- src/main/java/br/com/casadocodigo/redis/capitulo3/ArmazenarDadosDaMegaSena.java
+++ src/main/java/br/com/casadocodigo/redis/capitulo3/ArmazenarDadosDaMegaSena.java
@@ -8,11 +8,11 @@ public class ArmazenarDadosDaMegaSena {
 		String ganhadores = "22";
 		String dataSorteio = "09-11-2013";
 		String numeros = "8, 18, 26, 42, 56, 58";
-		String hash = String.format("resultado:%s:megasena", dataSorteio);
+		String chave = String.format("resultado:%s:megasena", dataSorteio);
 
 		Jedis jedis = new Jedis("localhost");
-		long resultado1 = jedis.hset(hash, "ganhadores", ganhadores);
-		long resultado2 = jedis.hset(hash, "numeros", numeros);
+		long resultado1 = jedis.hset(chave, "ganhadores", ganhadores);
+		long resultado2 = jedis.hset(chave, "numeros", numeros);
 		
 		String mensagem = String.format(
 				"Resultado 1 = %d, Resultado 2 = %d",
--- src/main/java/br/com/casadocodigo/redis/capitulo3/ObterDadosDaMegaSena.java
+++ src/main/java/br/com/casadocodigo/redis/capitulo3/ObterDadosDaMegaSena.java
@@ -5,11 +5,11 @@ import redis.clients.jedis.Jedis;
 public class ObterDadosDaMegaSena {
 
 	public static void main(String[] args) {
-		String hash = "resultado:09-11-2013:megasena";
+		String chave = "resultado:09-11-2013:megasena";
 		Jedis jedis = new Jedis("localhost");
 
-		String ganhadores = jedis.hget(hash, "ganhadores");
-		String numeros = jedis.hget(hash, "numeros");
+		String ganhadores = jedis.hget(chave, "ganhadores");
+		String numeros = jedis.hget(chave, "numeros");
 		
 		String mensagem = String.format(
 				"Ganhadores = %s, Numeros = [%s]",
