ccf792fbd2af29ac700c18ea8cdf854a8f3c0b29
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2013-12-03 23:23:50 -0200

    cap 4 - set

---
+++ src/main/java/br/com/casadocodigo/redis/capitulo4/ConjuntoDeRelacionamentoEntrePessoas.java
@@ -0,0 +1,64 @@
+package br.com.casadocodigo.redis.capitulo4;
+
+import java.util.Arrays;
+
+import redis.clients.jedis.Jedis;
+
+public class ConjuntoDeRelacionamentoEntrePessoas {
+
+	public void adicionaAmigos(String pessoa, String[] amigos) {
+		String chave = String.format("pessoas:%s:relacionamentos", pessoa);
+		Jedis jedis = new Jedis("localhost");
+		long resultado = jedis.sadd(chave, amigos);
+
+		System.out.println(
+				String.format(
+						"%s tem %d amigos %s", 
+						pessoa, 
+						resultado, 
+						Arrays.toString(amigos)
+				)
+		);
+	}
+
+	public static void main(String[] args) {
+		ConjuntoDeRelacionamentoEntrePessoas relacionamentos = 
+				new ConjuntoDeRelacionamentoEntrePessoas();
+
+		relacionamentos.adicionaAmigos(
+				"rafael", 
+				new String[] { "gustavo", "andressa", "rodrigo", "tereza" }
+		);
+		
+		relacionamentos.adicionaAmigos(
+				"andressa", 
+				new String[] { "cristiane", "rodrigo", "gustavo", "rafael" }
+		);
+		
+		relacionamentos.adicionaAmigos(
+				"gustavo", 
+				new String[] { "carlos", "tereza", "rafael", "andressa" }
+		);
+		
+		relacionamentos.adicionaAmigos(
+				"cristiane", 
+				new String[] { "tereza", "andressa", "carlos" }
+		);
+		
+		relacionamentos.adicionaAmigos(
+				"carlos", 
+				new String[] { "cristiane", "rodrigo", "gustavo" }
+		);
+		
+		relacionamentos.adicionaAmigos(
+				"rodrigo", 
+				new String[] { "andressa", "rafael", "carlos" }
+		);
+		
+		relacionamentos.adicionaAmigos(
+				"tereza", 
+				new String[] { "gustavo", "rafael", "cristiane" }
+		);
+	}
+
+}
