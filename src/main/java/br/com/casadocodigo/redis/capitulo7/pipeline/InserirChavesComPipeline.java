aacb26423407edbf3502d6d368be54d42e07293c
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2014-03-15 15:09:50 -0300

    cap 7 - pipeline

---
+++ src/main/java/br/com/casadocodigo/redis/capitulo7/pipeline/InserirChavesComPipeline.java
@@ -0,0 +1,23 @@
+package br.com.casadocodigo.redis.capitulo7.pipeline;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+
+public class InserirChavesComPipeline {
+
+	public static void main(String[] args) {
+		long tempoInicial = System.currentTimeMillis();
+		Jedis jedis = new Jedis("localhost");
+		Pipeline pipeline = jedis.pipelined();
+
+		for (int i = 1; i <= 100000; i++) {
+			pipeline.set("chave-" + i, String.valueOf(i));
+		}
+
+		pipeline.sync();
+
+		long tempoFinal = System.currentTimeMillis();
+		System.out.println(String.format("Tempo total: %.2f segundos", ((tempoFinal - tempoInicial) / 1000.0)));
+	}
+
+}
---
+++ src/main/java/br/com/casadocodigo/redis/capitulo7/pipeline/InserirChavesSequencialmente.java
@@ -0,0 +1,19 @@
+package br.com.casadocodigo.redis.capitulo7.pipeline;
+
+import redis.clients.jedis.Jedis;
+
+public class InserirChavesSequencialmente {
+
+	public static void main(String[] args) {
+		long tempoInicial = System.currentTimeMillis();
+		Jedis jedis = new Jedis("localhost");
+
+		for (int i = 1; i <= 100000; i++) {
+			jedis.set("chave-" + i, String.valueOf(i));
+		}
+
+		long tempoFinal = System.currentTimeMillis();
+		System.out.println(String.format("Tempo total: %.2f segundos", ((tempoFinal - tempoInicial) / 1000.0)));
+	}
+
+}
