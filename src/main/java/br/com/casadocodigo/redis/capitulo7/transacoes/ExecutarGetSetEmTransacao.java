71f59ed84643dec5d93afbb2c5cfd234c8bb9fb1
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2014-03-15 18:49:09 -0300

    cap 7 - multi

---
+++ src/main/java/br/com/casadocodigo/redis/capitulo7/transacoes/ExecutarGetSetEmTransacao.java
@@ -0,0 +1,41 @@
+package br.com.casadocodigo.redis.capitulo7.transacoes;
+
+import java.util.List;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
+public class ExecutarGetSetEmTransacao {
+
+	public String getSet(String chave, String novoValor) {
+		Jedis jedis = new Jedis("localhost");
+		Transaction transaction = jedis.multi();
+
+		transaction.get(chave);
+		transaction.set(chave, novoValor);
+
+		List<Object> resultados = transaction.exec();
+
+		return (String) resultados.get(0);
+	}
+
+	public static void main(String[] args) {
+		String valorNovo = "20";
+		String chave = "numero-de-acessos";
+
+		ExecutarGetSetEmTransacao transacao = 
+				new ExecutarGetSetEmTransacao();
+
+		String valorAntigo = transacao.getSet(chave, valorNovo);
+
+		System.out.println(
+			String.format(
+				"O valor antigo da chave %s é %s e o novo é %s", 
+				chave, 
+				valorAntigo, 
+				valorNovo
+			)
+		);
+	}
+
+}
